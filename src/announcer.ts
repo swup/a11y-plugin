46e22712752baeb5a97d56b0c3910ac80a64384a
Philipp Daun <post@philippdaun.net>
2024-04-05 15:00:06 +0200

    Minify announcer styles

--- src/announcer.ts
+++ src/announcer.ts
@@ -1,22 +1,9 @@
 import { createElement } from './util.js';
 
 export default class Announcer {
-	region: Element;
-
 	id: string = 'swup-announcer';
-
-	style: string = `
-		position: absolute;
-		top: 0;
-		left: 0;
-		clip: rect(0 0 0 0);
-		clip-path: inset(50%);
-		overflow: hidden;
-		white-space: nowrap;
-		word-wrap: normal;
-		width: 1px;
-		height: 1px;
-	`;
+	style: string = `position:absolute;top:0;left:0;clip:rect(0 0 0 0);clip-path:inset(50%);overflow:hidden;white-space:nowrap;word-wrap:normal;width:1px;height:1px;`;
+	region: Element;
 
 	constructor() {
 		this.region = this.getRegion() ?? this.createRegion();
