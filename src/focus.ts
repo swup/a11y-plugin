429958c94617bfccde7a72bde979eb1fc8a4ae1f
Philipp Daun <post@philippdaun.net>
2024-04-12 11:44:40 +0200

    Reorder focus module

--- src/focus.ts
+++ src/focus.ts
@@ -1,22 +1,3 @@
-export function getAutofocusElement(): HTMLElement | undefined {
-	const focusEl = document.querySelector<HTMLElement>('body [autofocus]');
-	if (focusEl && !focusEl.closest('inert, [aria-disabled], [aria-hidden="true"]')) {
-		return focusEl;
-	}
-}
-
-export function focusAutofocusElement(): boolean {
-	const autofocusEl = getAutofocusElement();
-	if (!autofocusEl) return false;
-
-	if (autofocusEl !== document.activeElement) {
-		// Only focus if not already focused
-		// No preventScroll flag here, as probably intended with autofocus
-		autofocusEl.focus();
-	}
-	return true;
-}
-
 export function focusElement(elementOrSelector: string | HTMLElement) {
 	let el: HTMLElement | null;
 	if (typeof elementOrSelector === 'string') {
@@ -36,5 +17,24 @@ export function focusElement(elementOrSelector: string | HTMLElement) {
 	} else {
 		// Removing the tabindex will reset screen reader position, so we'll keep it
 		// el.removeAttribute('tabindex');
+	}
+}
+
+export function focusAutofocusElement(): boolean {
+	const autofocusEl = getAutofocusElement();
+	if (!autofocusEl) return false;
+
+	if (autofocusEl !== document.activeElement) {
+		// Only focus if not already focused
+		// No preventScroll flag here, as probably intended with autofocus
+		autofocusEl.focus();
+	}
+	return true;
+}
+
+export function getAutofocusElement(): HTMLElement | undefined {
+	const focusEl = document.querySelector<HTMLElement>('body [autofocus]');
+	if (focusEl && !focusEl.closest('inert, [aria-disabled], [aria-hidden="true"]')) {
+		return focusEl;
 	}
 }
