eec6fa0642e3597a7b3c58d17bdba1062254237a
Philipp Daun <post@philippdaun.net>
2024-04-14 18:54:09 +0200

    Test utils

---
+++ tests/unit/createElement.test.ts
@@ -0,0 +1,19 @@
+import { describe, expect, it } from 'vitest';
+import { createElement } from '../../src/util.js';
+
+describe('createElement', () => {
+	it('should create an element from an html string', () => {
+			const html = '<div id="test" class="my-class">Hello World</div>';
+			const element = createElement(html);
+			expect(element).not.toBeNull();
+			expect(element.localName).toBe('div');
+			expect(element.id).toBe('test');
+			expect(element.className).toBe('my-class');
+			expect(element.textContent).toBe('Hello World');
+	});
+
+	it('should create nothing if no html is passed', () => {
+			const element = createElement('');
+			expect(element).toBeUndefined();
+	});
+});
